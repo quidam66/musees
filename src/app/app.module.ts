27b1a7fb71d848cc9739b888e74a02e772099c8d
Denis Archambault <quidam66@gmail.com>
2020-04-02 13:33:56 -0400

    ajout page accueil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule,
